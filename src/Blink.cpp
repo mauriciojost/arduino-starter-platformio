401c0f43a2bcc8d447665f9e9e0ecdfbc389912e
Mauricio Jost <mauricio.jost@gmail.com>
2016-09-17 09:57:34 +0200

    Add PWM and ADC to the example

--- src/Blink.cpp
+++ src/Blink.cpp
@@ -6,20 +6,32 @@
 
 #include <Arduino.h>
 
-void setup()
-{
+int val = 0;
+
+void setup() {
   // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  analogReference(INTERNAL);
+  Serial.begin(9600);
 }
 
-void loop()
-{
+void loop() {
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);
+  analogWrite(OUT_PWM, 155);
   // wait for a second
   delay(1000);
   // turn the LED off by making the voltage LOW
+  analogWrite(OUT_PWM, 10);
   digitalWrite(LED_BUILTIN, LOW);
    // wait for a second
-  delay(1000);
+  delay(500);
+
+  analogWrite(OUT_PWM, 255);
+  delay(500);
+
+
+
+  val = analogRead(A0);
+  Serial.println(val);
 }
